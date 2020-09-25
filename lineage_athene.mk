222521b884712163e10ac2af46e5efa2ece61792
Deivid21 <david.parra.ignacio@gmail.com>
2020-09-25 22:35:21 +0000

    lineage stuff

--- lineage_athene.mk
+++ lineage_athene.mk
@@ -18,7 +18,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_common.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from athene device
 $(call inherit-product, device/motorola/athene/device.mk)
