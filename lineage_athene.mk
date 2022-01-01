8618529656e3fcd556e5ccdec3697988615b2e82
psuryatej <80411701+psuryatej4android@users.noreply.github.com>
2022-01-01 19:18:16 +0800

    Comment the product/core and core_base.mk
    These files are not present in lineage-18.1 sources

--- lineage_athene.mk
+++ lineage_athene.mk
@@ -14,8 +14,8 @@
 # limitations under the License.
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_base.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/core.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
