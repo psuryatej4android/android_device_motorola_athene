98d76cc343208db41fc94a49eaa6026dea4d384e
psuryatej <80411701+psuryatej4android@users.noreply.github.com>
2022-01-17 11:40:58 +0800

    Update DRM Settings
    Removed drm 1.0-impl and service and update clearkey to 1.3 following the kiev 18.1 repo

--- hidl.mk
+++ hidl.mk
@@ -28,9 +28,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
-    android.hardware.drm@1.1-service.clearkey
+    android.hardware.drm@1.3-service.clearkey
 
 # Fingerprint
 PRODUCT_PACKAGES += \
